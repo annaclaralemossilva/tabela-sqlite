db4ea126b96996f2e60c9ac4465b32c67783e894
AlessandraLopesFerreyra <159266652+AlessandraLopesFerreyra@users.noreply.github.com>
2025-06-25 08:43:22 -0300

    Add files via upload

--- sql/2/alimentacao.sql
+++ sql/2/alimentacao.sql
@@ -4,18 +4,21 @@ VALUES (
     '120.123.456-19',
     '(45) 99999-999',
     'annaclaralemossilva@gmail.com',
-    '10/08/2008',
+    '2008.10.08',
     'Foz do Iguaçu - PR'
 );
 
-INSERT INTO forn (empresa, tipo_forn)
+INSERT INTO fornecedor (empresa, tipo_forn, cnpj_forn, tel_forn, end_forn)
 VALUES (
   	'Kibon',
-    'Sorvetes'
+    'Sorvetes',
+  	'00.623.904/0001-73',
+    '(45) 9986-9782',
+  	'Rua Izidoro Pastorello - Foz do Iguaçu - PR'
 );
 
 
-INSERT INTO funci ( id_funci, nome_funci, função, cpf_funci, rg_funci, tel_funci, email_funci, dtnasci_funci, dtcontr_funci, endereco_funci)
+INSERT INTO funcionario ( id_funci, nome_funci, função, cpf_funci, rg_funci, tel_funci, email_funci, dtnasci_funci, dtcontr_funci, endereco_funci)
 VALUES (
   	'Isadora',
     'dona',
@@ -23,8 +26,8 @@ VALUES (
     '00.000.000-0',
     '(45) 99999-999',
     'isadora@gmail.com',
-    '04/12/2007',
-    '10/03/2025',
+    '2007.04.12',
+    '2025.01.05',
     'Foz do Iguaçu - PR'
 );
 
@@ -47,5 +50,4 @@ INSERT INTO produto ( nome_prod, data_entrada, data_fabricacao, data_validade, q
     '3/06/2026',
     '50',
     'R$20,00',
-    'Kibon'
-   );
+    'Kibon' 
--- sql/2/criacao.sql
+++ sql/2/criacao.sql
@@ -1,20 +1,23 @@
-CREATE TABLE cliente (
+CREATE TABLE IF NOT EXISTS cliente (
     id_cli INTEGER PRIMARY KEY AUTOINCREMENT,
     nome_cli VARCHAR(100) NOT NULL,
   	cpf_cli VARCHAR(14) NOT NULL UNIQUE,
     tel_cli VARCHAR(15),
   	email_cli VARCHAR(100),
-  	dtnasc_cli TEXT,
+  	dtnasc_cli DATE,
   	endereco_cli TEXT
 );
 
-CREATE TABLE forn (
+CREATE TABLE IF NOT EXISTS fornecedor (
     id_forn INTEGER PRIMARY KEY AUTOINCREMENT,
     empresa VARCHAR(100) NOT NULL,
-  	tipo_forn varchar(30)
+  	tipo_forn varchar(30),
+  	cnpj_forn VARCHAR(18),
+ 	tel_forn VARCHAR(15),
+  	end_forn VARCHAR(100)
 );
 
-CREATE TABLE funci (
+CREATE TABLE IF NOT EXISTS funcionario (
     id_funci INTEGER PRIMARY KEY AUTOINCREMENT,
     nome_funci VARCHAR(100) NOT NULL,
   	função VARCHAR(50) NOT NULL,
@@ -22,16 +25,16 @@ CREATE TABLE funci (
   	rg_funci VARCHAR(12) NOT NULL UNIQUE,
     tel_funci VARCHAR(15),
   	email_funci VARCHAR(100),
-  	dtnasci_funci TEXT,
-  	dtcontr_funci TEXT,
+  	dtnasci_funci DATE,
+  	dtcontr_funci DATE,
   	endereco_funci TEXT
 );
 
-CREATE TABLE movimento (
+CREATE TABLE IF NOT EXISTS movimento (
     id_movi INTEGER PRIMARY KEY AUTOINCREMENT,
   	tipo_movi VARCHAR(100) NOT NULL,
   	categoria_movi VARCHAR(100) NOT NULL,
-  	data_movimento TEXT,
+  	data_movimento DATE,
   	valor_movi VARCHAR(10),
    	nome_funci VARCHAR(100) NOT NULL
 );
@@ -39,10 +42,20 @@ CREATE TABLE movimento (
 CREATE TABLE IF NOT EXISTS produto (
   	id_prod INTEGER PRIMARY KEY AUTOINCREMENT,
     nome_prod VARCHAR(100) NOT NULL,
-  	data_entrada TEXT,
- 	data_fabricacao TEXT,
-  	data_validade TEXT,
+  	data_entrada DATE,
+ 	data_fabricacao DATE,
+  	data_validade DATE,
   	quantidade_prod INTEGER,
   	valor_prod VARCHAR(10),
  	nome_forn VARCHAR(100) NOT NULL
   );
+  
+CREATE TABLE IF NOT EXISTS categoria (
+  );
+  
+CREATE TABLE IF NOT EXISTS caixa(
+  	id_caixa INTEGER PRIMARY KEY AUTOINCREMENT,
+  	FOREIGN KEY (id_prod) REFERENCES produto(id_prod),
+  );
+  
+
